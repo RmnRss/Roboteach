f001cdd46e742d9fb1ad4b503fc3e7757438b207
RmnRss <RmnRss@users.noreply.github.com>
2018-03-02 15:47:59 +0100

    Dernière version
    02/03/2018

--- Roboteach_mBlock/RoboTeach/js/RoboTeach.js
+++ Roboteach_mBlock/RoboTeach/js/RoboTeach.js
@@ -1,10 +1,17 @@
-// pilotageft.js
+/*
+  Nom : RoboTeach.js
+  Auteur : Romain Rousseau
+  Fonction : Fichier de pilotage du materiel RoboTeach sous mBlock en mode Scratch
+  Dernière modification : 02/03/2018
+*/
 
 (function(ext) {
   var device = null;
   var _rxBuf = [];
 
-  var moteurs = {
+  //---- Déclaration des variables ----//
+
+  var sorties = {
     M1:1,
     M2:2,
     M3:3,
@@ -32,11 +39,11 @@
     E8:8
   };
 
-  //pins des moteurs tels que MX = [ENX,INX,INX]
-  var pins_M1 = [13, 11, 12];
-  var pins_M2 = [8, 9, 10];
-  var pins_M3 = [7, 5, 6];
-  var pins_M4 = [2, 3, 4];
+  //pins des sorties tels que pins_MX = [ENX,INX,INX]
+  var pins_M1 = [6, 7, 4];
+  var pins_M2 = [5, 3, 2];
+  var pins_M3 = [11, 13, 12];
+  var pins_M4 = [10, 9, 8];
 
   //pin registre à décalage sur Arduino MEGA
   var pins_reg = [54, 55, 56]; //[DATA, LOAD, CLOCK]
@@ -44,19 +51,32 @@
   //sur Arduino UNO
   //var pins_reg = [14, 15, 16]; //[DATA, LOAD, CLOCK]
 
-  //Réinitialise les paramètres de la carte
+  //---- Fin de déclaration des variables ----//
+
+  //---- Définition des blocs fonction ----//
+
+  /*
+  30 : digital (binaire);
+  31 : analogique;
+  32 : PWM;
+
+  runPackage(numero de l'instruction à réaliser, pin d'écriture, état du pin souhaité);
+  getPackage(nextID, type d'info à récupérer, pin de lecture);
+  */
+
+  //Réinitialise les paramètres par défaut de la carte
   ext.resetAll = function(){
     device.send([0xff, 0x55, 2, 0, 4]);
   };
 
-  //Fonction d'Initialisation de l'Arduino
+  //Fonction d'initialisation de l'Arduino
   ext.runArduino = function(){
 		responseValue();
 	};
 
-  //Fonction qui réinitialise toute les valeurs à 0
+  //Fonction qui réinitialise toute les valeurs digitale ou de PWM à 0
   ext.arretUrgence = function(){
-    for (var i=0, i<=60, i++){
+    for (var i=0; i<=60; i++){
       runPackage(30,i,0);
       runPackage(32,i,0);
     }
@@ -64,10 +84,11 @@
 
   //Fonction qui permet de démarrer le moteur_select sélectionné dans le sens séléctionné
 	ext.demarrerMoteur = function(moteur_select,sens_select) {
+    var pwm_moteurs = 1250;
 
-    if (moteurs[moteur_select] == 1) {
+    if (sorties[moteur_select] == 1) {
 
-      runPackage(32,pins_M1[0],100); //pwm a 100
+      runPackage(32,pins_M1[0],pwm_moteurs); //pwm a 100
 
       if (sens[sens_select] == 1) {
         runPackage(30,pins_M1[1],1); //Sens horaire
@@ -78,9 +99,9 @@
       }
     }
 
-    if (moteurs[moteur_select] == 2) {
+    if (sorties[moteur_select] == 2) {
 
-      runPackage(32,pins_M2[0],100);
+      runPackage(32,pins_M2[0],pwm_moteurs);
 
       if (sens[sens_select] == 1) {
         runPackage(30,pins_M2[1],1);
@@ -91,9 +112,9 @@
       }
     }
 
-    if (moteurs[moteur_select] == 3) {
+    if (sorties[moteur_select] == 3) {
 
-      runPackage(32,pins_M3[0],100);
+      runPackage(32,pins_M3[0],pwm_moteurs);
 
       if (sens[sens_select] == 1) {
         runPackage(30,pins_M3[1],1);
@@ -105,8 +126,8 @@
 
     }
 
-    if (moteurs[moteur_select] == 4) {
-      runPackage(32,pins_M4[0],100);
+    if (sorties[moteur_select] == 4) {
+      runPackage(32,pins_M4[0],pwm_moteurs);
 
       if (sens[sens_select] == 1) {
         runPackage(30,pins_M4[1],1);
@@ -122,25 +143,133 @@
   //Réinitialise la valeur de la PWM et des sorties de controles à 0 en fonction du moteur_select choisi
 	ext.arreterMoteur = function(moteur_select){
 
-    if (moteurs[moteur_select] == 1) {
+    if (sorties[moteur_select] == 1) {
       runPackage(32,pins_M1[0],0);
       runPackage(30,pins_M1[1],0);
       runPackage(30,pins_M1[2],0);
     }
 
-    if (moteurs[moteur_select] == 2) {
+    if (sorties[moteur_select] == 2) {
       runPackage(32,pins_M2[0],0);
       runPackage(30,pins_M2[1],0);
       runPackage(30,pins_M2[2],0);
     }
 
-    if (moteurs[moteur_select] == 3) {
+    if (sorties[moteur_select] == 3) {
       runPackage(32,pins_M3[0],0);
       runPackage(30,pins_M3[1],0);
       runPackage(30,pins_M3[2],0);
     }
 
-    if (moteurs[moteur_select] == 4) {
+    if (sorties[moteur_select] == 4) {
+      runPackage(32,pins_M4[0],0);
+      runPackage(30,pins_M4[1],0);
+      runPackage(30,pins_M4[2],0);
+    }
+  };
+
+  ext.allumerLampe = function(sortie_select){
+    var pwm = 250;
+
+      if (sorties[sortie_select] == 1) {
+        runPackage(32,pins_M1[0],pwm);
+        runPackage(30,pins_M1[1],1);
+        runPackage(30,pins_M1[2],0);
+      }
+
+      if (sorties[sortie_select] == 2) {
+        runPackage(32,pins_M2[0],pwm);
+        runPackage(30,pins_M2[1],1);
+        runPackage(30,pins_M2[2],0);
+      }
+
+      if (sorties[sortie_select] == 3) {
+        runPackage(32,pins_M3[0],pwm);
+        runPackage(30,pins_M3[1],1);
+        runPackage(30,pins_M3[2],0);
+      }
+
+      if (sorties[sortie_select] == 4) {
+        runPackage(32,pins_M4[0],pwm);
+        runPackage(30,pins_M4[1],1);
+        runPackage(30,pins_M4[2],0);
+      }
+  };
+
+  ext.eteindreLampe = function(sortie_select){
+    if (sorties[sortie_select] == 1) {
+      runPackage(32,pins_M1[0],0);
+      runPackage(30,pins_M1[1],0);
+      runPackage(30,pins_M1[2],0);
+    }
+
+    if (sorties[sortie_select] == 2) {
+      runPackage(32,pins_M2[0],0);
+      runPackage(30,pins_M2[1],0);
+      runPackage(30,pins_M2[2],0);
+    }
+
+    if (sorties[sortie_select] == 3) {
+      runPackage(32,pins_M3[0],0);
+      runPackage(30,pins_M3[1],0);
+      runPackage(30,pins_M3[2],0);
+    }
+
+    if (sorties[sortie_select] == 4) {
+      runPackage(32,pins_M4[0],0);
+      runPackage(30,pins_M4[1],0);
+      runPackage(30,pins_M4[2],0);
+    }
+  };
+
+  ext.activerAimant = function(sortie_select){
+    var pwm = 1500;
+
+      if (sorties[sortie_select] == 1) {
+        runPackage(32,pins_M1[0],pwm);
+        runPackage(30,pins_M1[1],1);
+        runPackage(30,pins_M1[2],0);
+      }
+
+      if (sorties[sortie_select] == 2) {
+        runPackage(32,pins_M2[0],pwm);
+        runPackage(30,pins_M2[1],1);
+        runPackage(30,pins_M2[2],0);
+      }
+
+      if (sorties[sortie_select] == 3) {
+        runPackage(32,pins_M3[0],pwm);
+        runPackage(30,pins_M3[1],1);
+        runPackage(30,pins_M3[2],0);
+      }
+
+      if (sorties[sortie_select] == 4) {
+        runPackage(32,pins_M4[0],pwm);
+        runPackage(30,pins_M4[1],1);
+        runPackage(30,pins_M4[2],0);
+      }
+  };
+
+  ext.desactiverAimant = function(sortie_select){
+    if (sorties[sortie_select] == 1) {
+      runPackage(32,pins_M1[0],0);
+      runPackage(30,pins_M1[1],0);
+      runPackage(30,pins_M1[2],0);
+    }
+
+    if (sorties[sortie_select] == 2) {
+      runPackage(32,pins_M2[0],0);
+      runPackage(30,pins_M2[1],0);
+      runPackage(30,pins_M2[2],0);
+    }
+
+    if (sorties[sortie_select] == 3) {
+      runPackage(32,pins_M3[0],0);
+      runPackage(30,pins_M3[1],0);
+      runPackage(30,pins_M3[2],0);
+    }
+
+    if (sorties[sortie_select] == 4) {
       runPackage(32,pins_M4[0],0);
       runPackage(30,pins_M4[1],0);
       runPackage(30,pins_M4[2],0);
@@ -148,6 +277,26 @@
   };
 
 	ext.getBinaryIn = function(nextID, entree_binaire_select){
+    var deviceId = 30;
+
+    getPackage(nextID,deviceId,pins_reg[0]);
+
+    runPackage(30, pins_reg[1], 0); //digitalWrite(LOAD, LOW);
+    runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
+
+    waitMilliseconds(0.005);
+    runPackage(30, pins_reg[1], 1); //digitalWrite(LOAD, HIGH);
+    waitMilliseconds(0.005);
+
+    for (var i = 0 ; i<entrees_binaires[entree_binaire_select] ; ++i)
+    {
+      runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
+      runPackage(30, pins_reg[2], 0); //digitalWrite(CLOCK, LOW);
+    }
+
+    };
+
+  ext.getBinaryInBool = function(nextID, entree_binaire_select){
     var deviceId = 30;
 
     getPackage(nextID,deviceId,pins_reg[0]);
@@ -338,7 +487,7 @@
           tryNextDevice();
           return;
       }
-      device.set_receive_handler('Fischertechnik',function(data) {
+      device.set_receive_handler('RoboTeach',function(data) {
           processData(data);
       });
   };
@@ -354,11 +503,11 @@
   };
 
   ext._getStatus = function() {
-      if(!device) return {status: 1, msg: 'Fischertechnik disconnected'};
-      return {status: 2, msg: 'Fischertechnik connected'};
+      if(!device) return {status: 1, msg: 'RoboTeach disconnected'};
+      return {status: 2, msg: 'RoboTeach connected'};
   };
 
   var descriptor = {};
 
-	ScratchExtensions.register('Fischertechnik', descriptor, ext, {type: 'serial'});
+	ScratchExtensions.register('RoboTeach', descriptor, ext, {type: 'serial'});
 })({});
