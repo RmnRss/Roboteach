718a155c7cf1425ed38792ee21a3be9e91a59295
RmnRss <33030781+RmnRss@users.noreply.github.com>
2018-06-01 10:10:14 +0200

    Fichiers mBlock - Bug e-binaires 1bloc
    Fonctionnel sauf binaires qui demande 2 appuis

--- Roboteach_mBlock/RoboTeach/js/RoboTeach.js
+++ Roboteach_mBlock/RoboTeach/js/RoboTeach.js
@@ -1,8 +1,8 @@
 /*
   Nom : RoboTeach.js
   Auteur : Romain Rousseau
-  Fonction : Fichier de pilotage du materiel RoboTeach sous mBlock en mode Scratch
-  Dernière modification : 02/03/2018
+  Fonction : Fichier de pilotage du materiel RoboTeach sous mBlock 3.4 en mode Scratch
+  Dernière modification : 30/05/2018
 */
 
 (function(ext) {
@@ -276,32 +276,42 @@
     }
   };
 
-	ext.getBinaryIn = function(nextID, entree_binaire_select){
+	ext.getBinaryIn = function(nextID, entree_binaire_select)
+  {
     var deviceId = 30;
 
-    getPackage(nextID,deviceId,pins_reg[0]);
-
-    runPackage(30, pins_reg[1], 0); //digitalWrite(LOAD, LOW);
-    runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
-
-    waitMilliseconds(0.005);
-    runPackage(30, pins_reg[1], 1); //digitalWrite(LOAD, HIGH);
-    waitMilliseconds(0.005);
-
-    for (var i = 0 ; i<entrees_binaires[entree_binaire_select] ; ++i)
-    {
-      runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
-      runPackage(30, pins_reg[2], 0); //digitalWrite(CLOCK, LOW);
-    }
+    getPackage(nextID,deviceId,pins_reg[0]); //lecture du pin DATA
 
     };
 
-  ext.getBinaryInBool = function(nextID, entree_binaire_select){
+  ext.updateEntry = function(nextID, entree_binaire_select)
+    {
+      var deviceId = 30;
+
+      runPackage(30, pins_reg[1], 0); //digitalWrite(LOAD, LOW);
+      runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
+
+      waitMilliseconds(0.005);
+
+      runPackage(30, pins_reg[1], 1); //digitalWrite(LOAD, HIGH);
+
+      waitMilliseconds(0.005);
+
+      for (var i = 0 ; i<entrees_binaires[entree_binaire_select] ; ++i)
+        {
+          runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
+          runPackage(30, pins_reg[2], 0); //digitalWrite(CLOCK, LOW);
+        }
+
+    };
+
+  ext.getBinaryInBool = function(nextID, entree_binaire_select)
+  {
     var deviceId = 30;
 
     getPackage(nextID,deviceId,pins_reg[0]);
 
-    runPackage(30, pins_reg[1], 0); //digitalWrite(LOAD, LOW);
+    /*runPackage(30, pins_reg[1], 0); //digitalWrite(LOAD, LOW);
     runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
 
     waitMilliseconds(0.005);
@@ -311,10 +321,10 @@
     waitMilliseconds(0.005);
 
     for (var i = 0 ; i<entrees_binaires[entree_binaire_select] ; ++i)
-    {
-      runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
-      runPackage(30, pins_reg[2], 0); //digitalWrite(CLOCK, LOW);
-    }
+      {
+        runPackage(30, pins_reg[2], 1); //digitalWrite(CLOCK, HIGH);
+        runPackage(30, pins_reg[2], 0); //digitalWrite(CLOCK, LOW);
+      }*/
 
     };
 
@@ -355,111 +365,6 @@
     var nextID = arguments[0];
     Array.prototype.shift.call(arguments);
     sendPackage(arguments, 1);
-  }
-
-  function processData(bytes) {
-      trace(bytes);
-  }
-
-  var inputArray = [];
-  var _isParseStart = false;
-  var _isParseStartIndex = 0;
-  function processData(bytes) {
-    var len = bytes.length;
-    if(_rxBuf.length>30){
-      _rxBuf = [];
-    }
-    for(var index=0;index<bytes.length;index++){
-      var c = bytes[index];
-      _rxBuf.push(c);
-      if(_rxBuf.length>=2){
-        if(_rxBuf[_rxBuf.length-1]==0x55 && _rxBuf[_rxBuf.length-2]==0xff){
-          _isParseStart = true;
-          _isParseStartIndex = _rxBuf.length-2;
-        }
-        if(_rxBuf[_rxBuf.length-1]==0xa && _rxBuf[_rxBuf.length-2]==0xd&&_isParseStart){
-          _isParseStart = false;
-
-          var position = _isParseStartIndex+2;
-          var extId = _rxBuf[position];
-          position++;
-          var type = _rxBuf[position];
-          position++;
-          //1 byte 2 float 3 short 4 len+string 5 double
-          var value;
-          switch(type){
-            case 1:{
-              value = _rxBuf[position];
-              position++;
-            }
-              break;
-            case 2:{
-              value = readFloat(_rxBuf,position);
-              position+=4;
-              if(value<-255||value>1023){
-                value = 0;
-              }
-            }
-              break;
-            case 3:{
-              value = readInt(_rxBuf,position,2);
-              position+=2;
-            }
-              break;
-            case 4:{
-              var l = _rxBuf[position];
-              position++;
-              value = readString(_rxBuf,position,l);
-            }
-              break;
-            case 5:{
-              value = readDouble(_rxBuf,position);
-              position+=4;
-            }
-              break;
-            case 6:
-              value = readInt(_rxBuf,position,4);
-              position+=4;
-              break;
-          }
-          if(type<=6){
-            responseValue(extId,value);
-          }else{
-            responseValue();
-          }
-          _rxBuf = [];
-        }
-      }
-    }
-    }
-
-  function readFloat(arr,position){
-    var f= [arr[position],arr[position+1],arr[position+2],arr[position+3]];
-    return parseFloat(f);
-  }
-
-  function readInt(arr,position,count){
-    var result = 0;
-    for(var i=0; i<count; ++i){
-      result |= arr[position+i] << (i << 3);
-    }
-    return result;
-  }
-
-  function readDouble(arr,position){
-    return readFloat(arr,position);
-  }
-
-  function readString(arr,position,len){
-    var value = "";
-    for(var ii=0;ii<len;ii++){
-      value += String.fromCharCode(_rxBuf[ii+position]);
-    }
-    return value;
-  }
-
-  function appendBuffer( buffer1, buffer2 ) {
-      return buffer1.concat( buffer2 );
   }
 
   // Extension API interactions
